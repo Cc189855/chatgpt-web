a90a58e5da3ae739fe168b724d64d2386b332d18
Cc189855 <113840203+Cc189855@users.noreply.github.com>
2023-11-19 17:06:16 +0800

    Update env.js

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-envAPIEndpoint="https://api.717.buzz"
-envAPIKey="sk-MlqZwbmOKZHhQNs5CbBb66B2086944Fe9146205f1342A197"
+envAPIEndpoint="https://api.nextweb.fun/openai"
+envAPIKey="ak-yxausbSB2TexVsgy12QwDlCP7MDqKEX7BXGv3da9vISlkng6"
